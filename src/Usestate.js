5abecc3b93893fb76d3558d92d87fd2e27dcab60
sanjaydambal <sanjaydambal@gmail.com>
2024-03-04 12:42:25 +0530

    updated the details with setcar method

--- src/Usestate.js
+++ src/Usestate.js
@@ -1,21 +1,27 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useState } from 'react';
 
 const Usestate = () => {
-    const [color, setColor] = useState('red')
-    const [car,setCar] = useState({
-brand: 'BMW',
-model: '320i',
-year: 2020,
-color: 'blue'
-    })
-  return (
-    <div>
-        <h1 style={{color: color}}>Hello</h1>
-      <button type='button' onClick={() => setColor('blue')}>Click</button>
-      <h3> I am having a {car.brand} car whose model number is {car.model} , {car.color} in color manufactured in year {car.year} </h3>
-    </div>
-  )
-}
+    const [color, setColor] = useState('red');
+    const [car, setCar] = useState({
+        brand: 'BMW',
+        model: '320i',
+        year: 2020,
+        color: 'blue'
+    });
 
-export default Usestate
+    const updateColor = () => {
+        setCar(previousCar => {
+            return { ...previousCar, color: 'yellow',year: 2021 };
+        });
+    };
+
+    return (
+        <div>
+            <h1 style={{ color: color }}>Hello</h1>
+            <button type='button' onClick={updateColor}>Update Color</button>
+            <h3> I am having a {car.brand} car whose model number is {car.model}, {car.color} in color manufactured in year {car.year} </h3>
+        </div>
+    );
+};
+
+export default Usestate;
